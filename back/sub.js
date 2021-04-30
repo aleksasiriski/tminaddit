1bb43153a4bfd111704d554768927e7fc42102af
Anubi5555 <dragobegovic555@gmail.com>
2021-04-30 18:25:42 +0200

    Komentari i subredditi
    ispravka u semi

---
+++ back/sub.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+var subSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: String,
+    icon: String,
+    mainmoderator: String,
+    moderators: [String]
+}, { collection: "subs" });
+
+subSchema.pre("save", function (next) {
+    var currentDate = new Date();
+    this.updatedAt = currentDate;
+    if (!this.createdAt)
+        this.createdAt = currentDate;
+    next();
+});
+
+module.exports = mongoose.model("sub", subSchema);
