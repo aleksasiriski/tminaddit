ce303b32a9da801f7e2c43551bac2e24d978adfa
Aleksa Siriski <aleksasiriski@lavelektronik.com>
2021-05-04 23:23:56 +0200

    Server MongoDB

--- back/database/database.js
+++ back/database/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
     try {
-        const url = "mongodb://localhost:27017/tminaddit"
+        const url = "mongodb://server.lavelektronik.com:27017/tminaddit"
         const connection = await mongoose.connect(url, {
             useUnifiedTopology: true,
             useNewUrlParser: true,
