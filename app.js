637d87995228772ca48a08e8fe3b4eb2f310d0df
Aleksa Siriski <aleksasiriski@lavelektronik.com>
2021-05-07 20:45:49 +0200

    Visak include DM u app.js

--- app.js
+++ app.js
@@ -32,10 +32,6 @@ app.get("/register", checkNotAuthenticated, (req, res) => {
 const userRoute = require("./back/controller/user")
 app.use("/api", userRoute)
 
-// dms
-const dmRoute = require("./back/controller/dm")
-app.use("/api", dmRoute)
-
 // comments
 const commentRoute = require("./back/controller/comment")
 app.use("/api", commentRoute)
