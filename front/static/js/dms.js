8ee16a0cb23109bac80d61189383f09098f0d456
Aleksa Siriski <aleksasiriski@lavelektronik.com>
2021-05-09 22:06:44 +0200

    gotovi dm-ovi

--- front/static/js/dms.js
+++ front/static/js/dms.js
@@ -4,32 +4,28 @@ async function loadPage() {
     try {
         const dms = await axios.get("/api/dms")
         renderCards(dms.data.dms)
-        //addEventListeners() //ovo je dobro ali ne radi, jer je kod "SHREK" problem sto ne znam gde await da stavim pa se generisu DM-ovi nakon sto je pozvana funkcija za dodavanje button event-a
     } catch (err) {
         console.log(err)
     }
 }
 
-function addEventListeners() {
-    const messagesBtn = [...document.querySelectorAll("#info-button")]
-    messagesBtn.forEach((btn) =>
-        btn.addEventListener("click", () => {
-            window.location.href = `dm?id=${getId(btn)}`
-        })
-    )
-}
-
 function renderCards(dms) {
     const cards = document.querySelector("#dm-list")
-    //SHREK
     dms.forEach (async (dm) => {
         try {
             const user = await axios.get(`/api/username/${dm}`)
             cards.innerHTML += createCard(dm, user.data.username)
-            addEventListeners() //ovo je lose, jer se poziva na kreaciji svakog DM-a ali trenutno ovako jedino radi
+            addEventListener(dm)
         } catch (err) {
             console.log(err)
         }
+    })
+}
+
+function addEventListener(dm) {
+    const messageBtn = document.querySelector(`#dm-${dm}`)
+    messageBtn.addEventListener("click", () => {
+        window.location.href = `dm?id=${dm}`
     })
 }
 
@@ -37,7 +33,7 @@ function createCard(dm, username) {
     const card = `
     <div class="col-sm-4 text-center" dm-id="${dm}">
         <h2>${username}</h2>
-        <button id="info-button" type="button" class="btn btn-primary">View messages...</button>
+        <button id="dm-${dm}" type="button" class="btn btn-primary">View messages...</button>
         <br/><br/><br/>
     </div>`
     return card
