1d47789213b94b673fbde579848464eee2732acc
Aleksa Siriski <aleksasiriski@lavelektronik.com>
2021-05-16 23:20:52 +0200

    Delete sub

--- back/controller/sub.js
+++ back/controller/sub.js
@@ -73,29 +73,11 @@ router.put("/subs/:id", check.isAuthenticated, async (req, res) => {
         })
     }
 })
-
-app.put("/subs/:id/upVote", async (req, res) => {
-    try {
-        const id = req.params.id //uzet je ID iz URL-a
-        const specificSub = await sub.findById(id) 
-        specificSub.upVotes++
-        specificSub.save()
-        res.status(200).json({
-            success: true,
-        })
-    } catch (err) {
-        res.status(404).json({
-            success: false,
-            message: err.message
-        })
-    }
-})
-
-app.put("/subs/:id/downVote", async (req, res) => {
+router.put("/subs/:id/upvote", check.isAuthenticated, async (req, res) => {
     try {
         const id = req.params.id
         const specificSub = await sub.findById(id) 
-        specificSub.downVotes++
+        specificSub.upvotes++
         specificSub.save()
         res.status(200).json({
             success: true,
@@ -107,13 +89,36 @@ app.put("/subs/:id/downVote", async (req, res) => {
         })
     }
 })
-
+router.put("/subs/:id/downvote", check.isAuthenticated, async (req, res) => {
+    try {
+        const id = req.params.id
+        const specificSub = await sub.findById(id) 
+        specificSub.downvotes++
+        specificSub.save()
+        res.status(200).json({
+            success: true,
+        })
+    } catch (err) {
+        res.status(404).json({
+            success: false,
+            message: err.message
+        })
+    }
+})
 router.delete("/subs/:id", check.isAuthenticated, async (req, res) => {
     try {
         const subId = req.params.id
         const specificSub = await sub.findById(subId)
         if (isPermitted(specificSub, req.session.passport.user)) {
-            await specificSub.delete()
+            specificSub.themes.forEach(async (themeId) => {
+                const specificTheme = await theme.findById(themeId)
+                specificTheme.comments.forEach(async (commentId) => {
+                    const specificComment = await comment.findById(commentId)
+                    specificComment.delete()
+                })
+                specificTheme.delete()
+            })
+            specificSub.delete()
             res.status(200).json({
                 success: true
             })
