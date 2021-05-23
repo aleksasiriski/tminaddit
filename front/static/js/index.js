fb85a7fe3e6633f12554e37d16c6cab7a8c0ee8e
sarakojic <63716933+sarakojic@users.noreply.github.com>
2021-05-23 18:25:30 +0200

    ispravljene greske

--- front/static/js/index.js
+++ front/static/js/index.js
@@ -1,16 +1,18 @@
+const { post } = require("../../../back/controller/theme");
+
 GetData();
 
 async function GetData() {
     try {
-        let phones = await axios.get("/api/phones");
-        RenderCards(phones.data.phones);
+        let themes = await axios.get("/api/themes");
+        RenderCards(themes.data.themes);
         AddEventListeners();
     } catch (err) {
         console.log(err);
     }
 }
 
-function AddEventListeners() {
+/*function AddEventListeners() {
     let moreInfoBtn = [...document.querySelectorAll(".info-button")];
     moreInfoBtn.forEach((btn) =>
         btn.addEventListener("click", () => {
@@ -22,34 +24,68 @@ function AddEventListeners() {
     deleteBtns.forEach((btn) =>
         btn.addEventListener("click", () => DeleteData(btn))
     );
-}
+}*/
 
-function RenderCards(phones) {
-    const cardsDiv = document.querySelector("#phone-list");
+function RenderCards(themes) {
+    const cardsDiv = document.querySelector("#themes");
     let cards = "";
-    phones.forEach((phone) => {
-        cards += CreateCard(phone);
+    themes.forEach((theme) => {
+        themes += CreateCard(theme);
     });
 
     cardsDiv.innerHTML = cards;
 }
 
-function CreateCard(phone) {
-
+function CreateCard(theme) {
     let card = `
-    <div class="col-sm-4 text-center" phone-id="${phone._id}">
-        <img src="${phone.image}" alt="${phone.model}">
-        <h2>Model: ${phone.model}</h2>
-        <p>Manufacturer: ${phone.manufacturer}</p>
-        <button type="button" class="btn btn-primary info-button">Read more...</button>
-        <button type="button" class="btn btn-secondary delete-button">Delete</button>
-        <br/><br/><br/>
-    </div>
-    `;
+                    <div class="card gedf-card main-card" theme-id="${theme._id}">
+                      <div class="card-header">
+                          <div class="d-flex justify-content-between align-items-center">
+                              <div class="d-flex justify-content-between align-items-center">
+                                  <div class="mr-2">
+                                      <img class="rounded-circle" width="45" src="https://picsum.photos/50/50" alt="">
+                                  </div>
+                                  <div class="ml-2">
+                                      <div class="h5 m-0">${theme.author}</div>
+                                      <a href="sub"><div class="h7 text-muted">${theme.sub}</div></a>
+                                  </div>
+                              </div>
+                              <div>
+                                  <div class="dropdown">
+                                      <button class="btn btn-link dropdown-toggle" type="button" id="gedf-drop1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                          <i class="fa fa-ellipsis-h"></i>
+                                      </button>
+                                      <div class="dropdown-menu dropdown-menu-right" aria-labelledby="gedf-drop1">
+                                          <div class="h6 dropdown-header">Configuration</div>
+                                          <a class="dropdown-item" href="#">Save</a>
+                                          <a class="dropdown-item" href="#">Hide</a>
+                                          <a class="dropdown-item" href="#">Report</a>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                      <div class="card-body">
+                          <div class="text-muted h7 mb-2"> <i class="fa fa-clock-o"></i>${theme.createdAt}</div>
+                          <a class="card-link" href="theme">
+                              <h5 class="card-title">${theme.title}.</h5>
+                          </a>
 
+                          <p class="card-text">
+                            ${theme.content}
+                          </p>
+                      </div>
+                      <div class="card-footer">
+                          <button class="transparent-btn card-link"><i class="fa fa-arrow-up"></i> 23</button>
+                          <button class="transparent-btn card-link"><i class="fa fa-arrow-down"></i> </button>
+                          <a href="theme?id=${theme.id}"><button class="transparent-btn card-link"onclick="showComment()"><i class="fa fa-comment"></i> Comment</button></a>
+                          <button class="transparent-btn card-link" onclick="share()"><i class="fa fa-mail-forward"></i> Share</button>
+                          <button class="transparent-btn card-link" ><i class="fa fa-bookmark"></i> Save</button>
+                      </div>
+                  </div>`
     return card;
 }
-
+/*
 async function DeleteData(btn) {
     const id = getId(btn);
     try {
@@ -58,10 +94,49 @@ async function DeleteData(btn) {
     } catch (err) {
         console.log(err);
     }
-}
+}*/
+/*
 
 function getId(btn) {
     const parent = btn.parentElement;
     const id = parent.getAttribute("phone-id");
     return id;
+}*/
+const postButton = document.querySelector("#btn-post")
+postButton.addEventListener("click", getInput)
+async function getInput() {
+    try {
+        post=document.querySelector("#posts")
+        image=document.querySelector("#images")
+        link=document.querySelector("#links")
+        if (post.hasAttribute("active"))
+        {
+            const title = document.querySelector("#title")
+            const content = document.querySelector("#content")
+            await axios.post("/api/themes", newTheme)
+            title.value = ""
+            content.value = ""
+            location.reload()
+        }
+        if (image.hasAttribute("active"))
+        {
+            const title = document.querySelector("#title-file")
+            const content = document.querySelector("#customFile")
+            await axios.post("/api/themes", newTheme)
+            title.value = ""
+            content.value = ""
+            location.reload()
+        }
+        if (link.hasAttribute("active"))
+        {
+            const title = document.querySelector("#title-link")
+            const content = document.querySelector("#link")
+            await axios.post("/api/themes", newTheme)
+            title.value = ""
+            content.value = ""
+            location.reload()
+        }
+    } catch (err) {
+        console.log(err)
+    }
 }
